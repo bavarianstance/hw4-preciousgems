fe0901af096531d9df203f5411e4649e57afc5b1
bavarianstance <bavarianstance@gmail.com>
2019-02-27 17:16:05 -0800

    beta v0.5

---
+++ assets/javascript/game.js
@@ -0,0 +1,58 @@
+let wins = 0;
+let losses = 0;
+let targetScore;
+let score;
+let gemValues;
+
+
+  let resetGame = () => {
+    gemValues = [];
+    score = 0;
+    targetScore = Math.floor(Math.random() * 100);
+    for (let x = 0; x < 8; x++){
+    gemValues.push(Math.floor(Math.random() * 10));
+    };
+    $("#target-score").text(targetScore);
+  };
+  resetGame();
+
+  let gemImgs =["gem1", "gem2", "gem3", "gem4", "gem5", "gem6", "gem7", "gem8"]
+
+  for (let i = 0; i < gemValues.length; i++) {
+
+    let gemImage = $("<img>");
+
+    gemImage.addClass("gem-image");
+
+    gemImage.attr("src", ("./assets/images/" + gemImgs[i] +".png" ))
+
+    gemImage.attr("data-gemvalue", gemValues[i]);
+
+    $("#gems").append(gemImage);
+  }
+
+  $(".gem-image").on("click", function() {
+
+
+    let gemValue = ($(this).attr("data-gemvalue"));
+    gemValue = parseInt(gemValue);
+    score += gemValue;
+
+
+    if (score === targetScore) {
+      wins++;
+      resetGame();
+    }
+
+    else if (score >= targetScore) {
+      losses++;
+      resetGame();
+    }
+
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+    $("#your-score").text(score);
+
+  });
+
+ 
