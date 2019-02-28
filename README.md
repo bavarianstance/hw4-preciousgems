fe7d2fd15f689f22874748495c8358557ba85a62
bavarianstance <bavarianstance@gmail.com>
2019-02-27 21:54:50 -0800

    v2 upload

--- README.md
+++ README.md
@@ -6,11 +6,16 @@
 * HTML
 * CSS
 * Animate.CSS
+* Bootstrap V4
 
 ## Comments
-* known bug in jQuery preventing gemValues from updating after reset, trying to rectify
+* known bug in jQuery preventing gemValues from updating after reset (fixed in v2)
+* reset() updates .gem-image.attr "data-gemvalue" FINALLY FIXED!!!
 * utilized arrays to dynamically generate random values for gem values 
-* JS ES6 syntax
+* utilize JS ES6 syntax
+* reset() function resets score, gemValues array, targetScore
+* added Counter
+* added edge case for #gem7
 
 ## Screenshot
 ![screenshot](./assets/images/screenshot.png "screenshot")
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,15 +3,19 @@ let losses = 0;
 let targetScore;
 let score;
 let gemValues;
-let gemImgs =["gem1", "gem2", "gem3", "gem4", "gem5", "gem6", "gem7", "gem8"]
+let count;
+let gemImgs =["gem1", "gem2", "gem3", "gem4", "gem5", "gem6", "gem7"]
 
 
   let resetGame = () => {
     gemValues = [];
     score = 0;
+    count = 0;
     targetScore = Math.floor(Math.random() * 100);
-    for (let x = 0; x < 8; x++){
+    for (let x = 0; x < 7; x++){
     gemValues.push(Math.floor(Math.random() * 10));
+    $("#gem" + x).attr("data-gemvalue", gemValues[x]);
+    $("#gem7").attr("data-gemvalue", gemValues[x]);
     };
     $("#target-score").text(targetScore);
   };
@@ -22,6 +26,7 @@ let newGame = () => {
     gemImage.addClass("gem-image");
     gemImage.attr("src", ("./assets/images/" + gemImgs[i] +".png" ))
     gemImage.attr("data-gemvalue", gemValues[i]);
+    gemImage.attr("id", gemImgs[i]);
     $("#gems").append(gemImage);
   }
 };
@@ -31,11 +36,11 @@ let newGame = () => {
 
   $(".gem-image").on("click", function() {
 
-
     let gemValue = ($(this).attr("data-gemvalue"));
 
     gemValue = parseInt(gemValue);
     score += gemValue;
+    count++
 
 
     if (score === targetScore) {
@@ -46,10 +51,12 @@ let newGame = () => {
     else if (score >= targetScore) {
       losses++;
       resetGame();
+
     }
 
     $("#wins").text(wins);
     $("#losses").text(losses);
+    $("#count").text(count);
     $("#your-score").text(score);
 
   });
